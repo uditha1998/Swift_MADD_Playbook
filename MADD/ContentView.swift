1711a6dc3706fa705a015a73f69fb97deb8cadef
Isiwara Uditha <61772438+uditha1998@users.noreply.github.com>
2024-04-17 21:53:02 +0530

    Setup Workspace for MADD

---
+++ MADD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MADD
+//
+//  Created by Isiwara Uditha on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Heo, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MADD/MADDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MADDApp.swift
+//  MADD
+//
+//  Created by Isiwara Uditha on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MADDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
